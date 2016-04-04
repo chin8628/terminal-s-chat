a13fa5228165b5d7c14c3e2286f509ffbde8ef6f
chin8628 <boon.8628@hotmail.com>
2016-04-04 18:49:37 +0700

    Use connection.h library instead of original function

--- socket_client.c
+++ socket_client.c
@@ -1,53 +1,49 @@
 #include <stdio.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include "connection.h"
 
 #define LENGHT_MESSAGE 255
 
 int main(int argc , char *argv[]) {
-    int read_size;
-    char message[LENGHT_MESSAGE] , server_reply[LENGHT_MESSAGE], c;
 
-    initial_connection("127.0.0.1", 8888)
+    int read_size;
+    char message_buffer[LENGHT_MESSAGE], c;
+
+    initial_connection("127.0.0.1", 8888);
 
     puts("\n-------------------------------");
     puts("   Welcome to terminal chat!");
     puts("-------------------------------\n");
 
-    read_size = recv(socket_desc, server_reply , 2000 , 0);
-    server_reply[read_size] = '\0';
-    printf("%s\n", server_reply);
+    recieve_data(LENGHT_MESSAGE, message_buffer);
+    puts(message_buffer);
 
     printf("Username: ");
-    scanf(" %[^\n]s", message);
-    if( send(socket_desc , message , strlen(message) , 0) < 0)
+    scanf(" %[^\n]s", message_buffer);
+    if(send_data(message_buffer))
         puts("Send failed");
 
     printf("Chat with: ");
-    scanf(" %[^\n]s", message);
-    if( send(socket_desc , message , strlen(message) , 0) < 0)
+    scanf(" %[^\n]s", message_buffer);
+    if(send_data(message_buffer))
         puts("Send failed");
 
     //Send some data
     do {
-        //Reset value in message for check while loop's condition
-        strcpy(message, "");
+        //Reset value in message_buffer for check while loop's condition
+        strcpy(message_buffer, "");
 
         //Send
         printf("You > ");
-        scanf(" %[^\n]s", message);
-        if( send(socket_desc , message , strlen(message) , 0) < 0)
+        scanf(" %[^\n]s", message_buffer);
+        if(send_data(message_buffer))
             puts("Send failed");
 
         //Receive a reply from the server
-        if( (read_size = recv(socket_desc, server_reply , 2000 , 0)) < 0)
-            puts("recv failed");
-        server_reply[read_size] = '\0';
-        puts(server_reply);
+        recieve_data(LENGHT_MESSAGE, message_buffer);
+        puts(message_buffer);
 
-    } while(strcmp(message, ":q!") != 0);
+    } while(strcmp(message_buffer, ":q!") != 0);
 
     puts("\n------------------------------");
     puts("Good bye, see you again! owo)/\n");
